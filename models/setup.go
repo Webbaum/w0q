59bf9a3084cd2a40ec528766a58c018c162985d1
Ferdinand Mütsch <ferdinand@muetsch.io>
2021-04-19 22:37:33 +0200

    fix: properly read database path

--- models/setup.go
+++ models/setup.go
@@ -1,16 +1,15 @@
 package models
 
 import (
+	"github.com/Webbaum/w0q/config"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"os"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	var path = os.Getenv("W0Q_DB_PATH")
-	database, err := gorm.Open("sqlite3", path)
+	database, err := gorm.Open("sqlite3", config.MyConfig.DBPath)
 
 	if err != nil {
 		panic("Failed to connect to database: " + err.Error())
